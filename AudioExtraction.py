54f216a2da6b1964a7fa87987d1315d2676263cb
Mousam Dey <76545841+Mousam98@users.noreply.github.com>
2020-12-29 11:58:23 +0530

    Add files via upload
    This python script is just to describe the basics of audio file manipulation which consists of different types of music cultures or genres.

---
+++ AudioExtraction.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 29 09:58:16 2020
+
+@author: Mousam
+"""
+
+
+import librosa
+import librosa.display
+import IPython.display as ipd
+
+blues = 'blues/blues.00002.wav'
+jazz = 'jazz/jazz.00008.wav'
+ragge = 'raggae/raggae.00004.wav'
+
+ipd.Audio(blues)
